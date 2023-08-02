65024367ce4d4dd5362078619e299129c9293496
Saiful Islam <saifulsaif321@gmail.com>
2023-08-03 00:06:00 +0600

    feat: core network module

--- core/src/main/java/com/saiful/core/di/CoreNetworkModule.kt
+++ core/src/main/java/com/saiful/core/di/CoreNetworkModule.kt
@@ -1,8 +1,16 @@
 package com.saiful.core.di
 
+import com.saiful.core.data.RequestInterceptor
+import com.saiful.core.data.ResponseInterceptor
+import com.saiful.core.di.qualifiers.BaseUrl
+import com.saiful.core.di.qualifiers.GenericErrorMessage
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.Provides
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
 
 object CoreNetworkModule {
 
@@ -10,6 +18,48 @@ object CoreNetworkModule {
     fun provideMoshi(): Moshi {
         return Moshi.Builder()
             .addLast(KotlinJsonAdapterFactory())
+            .build()
+    }
+
+    @Provides
+    fun requestInterceptor(): RequestInterceptor {
+        return RequestInterceptor()
+    }
+
+    @Provides
+    fun responseInterceptor(
+        moshi: Moshi,
+        @GenericErrorMessage genericErrorMessage: String
+    ): ResponseInterceptor {
+        return ResponseInterceptor(
+            moshi = moshi,
+            genericErrorMsg = genericErrorMessage
+        )
+    }
+
+    @Provides
+    fun provideOKHttpClient(
+        requestInterceptor: RequestInterceptor,
+        responseInterceptor: ResponseInterceptor
+    ): OkHttpClient {
+        return OkHttpClient.Builder()
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
+            .addInterceptor(requestInterceptor)
+            .addInterceptor(responseInterceptor)
+            .build()
+    }
+
+    @Provides
+    fun provideRetrofit(
+        moshi: Moshi,
+        okHttpClient: OkHttpClient,
+        @BaseUrl baseUrl: String
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
             .build()
     }
 }
