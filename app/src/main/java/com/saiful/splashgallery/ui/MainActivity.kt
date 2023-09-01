77cabb30573b68302a4ce0e630a328307ed3ddb3
Saiful Islam <saifulsaif321@gmail.com>
2023-09-02 00:46:40 +0600

    feat: splash screen (ongoing)

--- app/src/main/java/com/saiful/splashgallery/ui/MainActivity.kt
+++ app/src/main/java/com/saiful/splashgallery/ui/MainActivity.kt
@@ -10,12 +10,16 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.saiful.core.components.logger.log
 import com.saiful.splashgallery.ui.theme.SplashGalleryTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen()
+
         setContent {
             SplashGalleryTheme {
                 // A surface container using the 'background' color from the theme
