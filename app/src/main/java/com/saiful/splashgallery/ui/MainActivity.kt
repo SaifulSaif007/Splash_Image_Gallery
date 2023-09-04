1d519d2164c0db11adcfdfcd257bd83ae548a8a9
Saiful Islam <saifulsaif321@gmail.com>
2023-09-04 23:12:42 +0600

    feat: splash screen

--- app/src/main/java/com/saiful/splashgallery/ui/MainActivity.kt
+++ app/src/main/java/com/saiful/splashgallery/ui/MainActivity.kt
@@ -6,12 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import com.saiful.core.components.logger.log
 import com.saiful.splashgallery.ui.theme.SplashGalleryTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,32 +18,14 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             SplashGalleryTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
-                    log(msg = "Hello there")
+                    //Home NavGraph here
 
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SplashGalleryTheme {
-        Greeting("Android")
     }
 }
