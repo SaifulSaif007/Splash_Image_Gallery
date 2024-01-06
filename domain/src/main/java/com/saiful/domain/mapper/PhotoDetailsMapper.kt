0c486154edbd5ae8b3ee44276cadfbf6c5022811
Saiful Islam <saifulsaif321@gmail.com>
2024-01-06 20:47:25 +0600

    feat[ui update]: add number formatter
    Signed-off-by: Saiful Islam <saifulsaif321@gmail.com>

--- domain/src/main/java/com/saiful/domain/mapper/PhotoDetailsMapper.kt
+++ domain/src/main/java/com/saiful/domain/mapper/PhotoDetailsMapper.kt
@@ -3,23 +3,44 @@ package com.saiful.domain.mapper
 import com.saiful.data.model.photo.details.PhotoDetails
 import com.saiful.data.utils.UNKNOWN
 import com.saiful.domain.model.PhotoDetailsItem
+import kotlin.math.ln
+import kotlin.math.pow
 
 internal fun PhotoDetails.toPhotoDetailsItem() =
     PhotoDetailsItem(
-        profileName = this.user.name,
-        profileImage = this.user.profileImage.small,
-        mainImage = this.urls.regular,
-        thumbnailImage = this.urls.thumb,
+        profileName = user.name,
+        profileImage = user.profileImage.small,
+        mainImage = urls.regular,
+        thumbnailImage = urls.thumb,
         camera = (exif.model ?: UNKNOWN).trim(),
         aperture = if (exif.aperture != null) "f/${exif.aperture}" else UNKNOWN,
         focalLength = if (exif.focalLength != null) "${exif.focalLength}mm" else UNKNOWN,
         shutterSpeed = if (exif.exposureTime != null) "${exif.exposureTime}s" else UNKNOWN,
         iso = if (exif.iso != null) "${exif.iso}" else UNKNOWN,
-        views = "$views",
-        dimensions = this.width.toString() + " x " + this.height.toString(),
-        downloads = this.downloads.toString(),
-        likes = this.likes.toString(),
-        tags = this.tags.map { tag ->
+        views = views.formatNumberWithSuffix(),
+        dimensions = "$width x $height",
+        downloads = downloads.formatNumberWithSuffix(),
+        likes = likes.formatNumberWithSuffix(),
+        tags = tags.map { tag ->
             tag.title
         }
     )
+
+
+fun Long.formatNumberWithSuffix(): String {
+    if (this < 1_000) {
+        return this.toString()
+    }
+
+    val exp = (ln(this.toDouble()) / ln(1_000.0)).toInt()
+    val formattedValue = String.format("%.1f", this / 1_000.0.pow(exp.toDouble()))
+
+    val suffix = when (exp) {
+        1 -> "K"
+        2 -> "M"
+        3 -> "B"
+        else -> ""
+    }
+
+    return "$formattedValue$suffix"
+}
