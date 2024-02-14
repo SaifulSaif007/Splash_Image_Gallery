0e1e8e924e8a287b7b9dd4008fa04e4ef56acf1e
Saiful Islam <saifulsaif321@gmail.com>
2024-02-15 00:27:11 +0600

    feat[profile]: profile photos usecase with testcase
    Signed-off-by: Saiful Islam <saifulsaif321@gmail.com>

---
+++ domain/src/main/java/com/saiful/domain/usecase/GetProfilePhotosUseCase.kt
@@ -0,0 +1,17 @@
+package com.saiful.domain.usecase
+
+import androidx.paging.PagingData
+import com.saiful.core.domain.UseCase
+import com.saiful.data.repository.profile.ProfileRepository
+import com.saiful.domain.mapper.toPhotoItem
+import com.saiful.domain.model.PhotoItem
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetProfilePhotosUseCase @Inject constructor(
+    private val profileRepository: ProfileRepository
+) : UseCase<String, Flow<PagingData<PhotoItem>>>() {
+    override suspend fun execute(params: String): Flow<PagingData<PhotoItem>> {
+        return profileRepository.profilePhotos(params).toPhotoItem()
+    }
+}
